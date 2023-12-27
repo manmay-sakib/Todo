db50428b01397f99e3cfb74113af112f1835239a
manmay sakib <monmy.sakib@gmail.com>
2023-12-27 18:18:44 +0600

    added retrofit, api, di module functions, domain model, mappers.

--- app/src/main/java/com/example/todo/feature_todo/data/di/TodoModule.kt
+++ app/src/main/java/com/example/todo/feature_todo/data/di/TodoModule.kt
@@ -4,18 +4,40 @@ import android.content.Context
 import androidx.room.Room
 import com.example.todo.feature_todo.data.local.TodoDatabase
 import com.example.todo.feature_todo.data.local.dao.TodoDao
+import com.example.todo.feature_todo.data.remote.api.TodoApi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ActivityContext
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 import javax.inject.Singleton
 
 
 @Module
 @InstallIn(SingletonComponent::class)
 object TodoModule {
+
+    @Provides
+    fun provideRetrofitApi(retrofit: Retrofit): TodoApi{
+        return retrofit.create(TodoApi::class.java)
+    }
+
+
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(): Retrofit{
+        return Retrofit.Builder()
+            .addConverterFactory(
+                GsonConverterFactory.create()
+            )
+            .baseUrl("https://todo-425b7-default-rtdb.firebaseio.com/")
+            .build()
+    }
 
     // by this function we are telling dagger hilt to
     // provide dao instances
