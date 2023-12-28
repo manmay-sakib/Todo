e967637b7e1cf6218b4331b39587a83327fa4dd6
manmay sakib <monmy.sakib@gmail.com>
2023-12-28 18:04:53 +0600

    building out the domain layer.

---
+++ app/src/main/java/com/example/todo/core/util/TodoConstants.kt
@@ -0,0 +1,5 @@
+package com.example.todo.core.util
+
+object TodoUseCasesStrings{
+    const val EMPTY_TITLE_OR_DESCRIPTION = "Both the todo title and description must be populated"
+}
---
+++ app/src/main/java/com/example/todo/feature_todo/domain/util/TodoItemOrder.kt
@@ -0,0 +1,40 @@
+package com.example.todo.feature_todo.domain.util
+
+sealed class TodoItemOrder(
+    val sortingDirection: SortingDirection,
+    val showArchived: Boolean
+){
+    class Title(
+        sortingDirection: SortingDirection,
+        showArchived: Boolean
+        ) : TodoItemOrder(
+            sortingDirection,
+            showArchived
+        )
+    class Time(
+        sortingDirection: SortingDirection,
+        showArchived: Boolean
+    ) : TodoItemOrder(
+        sortingDirection,
+        showArchived
+    )
+    class Completed(
+        sortingDirection: SortingDirection,
+        showArchived: Boolean
+    ) : TodoItemOrder(
+        sortingDirection,
+        showArchived
+    )
+
+    fun copy(
+        sortingDirection: SortingDirection,
+        showArchived: Boolean
+    ): TodoItemOrder{
+        return when(this){
+            is Title -> Title(sortingDirection, showArchived)
+            is Time -> Title(sortingDirection, showArchived)
+            is Completed -> Title(sortingDirection, showArchived)
+        }
+    }
+
+}
